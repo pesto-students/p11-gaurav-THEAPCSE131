2c6789b07ff4654b75722fd85049d14419ca2ea3
Abhishek Pathak <abhishek.pathak@smartdocs.ai>
2023-05-14 15:02:05 +0530

    Number to Guess - Done

--- Week-3/JavaScript Fundamentals/JavaScript number guessing game/script.js
+++ Week-3/JavaScript Fundamentals/JavaScript number guessing game/script.js
@@ -4,35 +4,28 @@ function playGuessingGame(numToGuess, totalGuesses) {
   } else {
     let guessedNum = 0;
     guessedNum = prompt("Enter a number between1 and 100");
-    if (guessedNum === numToGuess) {
-      console.log(guessedNum, "SSS");
-      alert("Hi");
-      alert(`Congratulations! You guessed the number ${numToGuess} correctly.`);
-      return guessedNum;
-    } else {
-      while (totalGuesses > 0) {
-        if (guessedNum < numToGuess) {
-          guessedNum = prompt(
-            guessedNum + " is too small. Guess a larger number"
-          );
-          //   return guessedNum;
-        } else if (guessedNum > numToGuess) {
-          guessedNum = prompt(
-            guessedNum + " is too large. Guess a smaller number."
-          );
-          //   return guessedNum;
-        } else if (guessedNum === "") {
-          guessedNum = prompt("Please enter a number");
-          //   return guessedNum;
-        } else {
-          alert(
-            `Congratulations! You guessed the number ${numToGuess} correctly.`
-          );
-          return numToGuess;
-        }
-
-        totalGuesses--;
+    while (totalGuesses > 0) {
+      if (guessedNum < numToGuess) {
+        guessedNum = prompt(
+          guessedNum + " is too small. Guess a larger number"
+        );
+        //   return guessedNum;
+      } else if (guessedNum > numToGuess) {
+        guessedNum = prompt(
+          guessedNum + " is too large. Guess a smaller number."
+        );
+        //   return guessedNum;
+      } else if (guessedNum === "") {
+        guessedNum = prompt("Please enter a number");
+        //   return guessedNum;
+      } else {
+        alert(
+          `Congratulations! You guessed the number ${numToGuess} correctly.`
+        );
+        return numToGuess;
       }
+
+      totalGuesses--;
     }
   }
   alert(`Game over. You couldn't guess the number ${numToGuess}.`);
